3ecbf9ba8a0f71237f3d15673f6bf54d546d2f8a
Allan Davis <cajun.code@gmail.com>
2019-08-21 22:42:51 -0400

    Set Screen Data

--- lib/breaker.dart
+++ lib/breaker.dart
@@ -1,6 +1,16 @@
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/flame.dart';
 
-class Breaker{
-  
+class Breaker extends Game{
+  @override
+  void render(Canvas canvas) {
+    // TODO: implement render
+  }
+
+  @override
+  void update(double t) {
+    // TODO: implement update
+  }
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flame/util.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/flame.dart';
-
+import 'package:flutter/services.dart';
+import 'breaker.dart';
 
 void main() async {
   Util flameUtil = Util();
-  await flameUtil.
+  await flameUtil.fullScreen();
+  await flameUtil.setOrientation(DeviceOrientation.portraitUp);
+
+  //Breaker
 }
 
